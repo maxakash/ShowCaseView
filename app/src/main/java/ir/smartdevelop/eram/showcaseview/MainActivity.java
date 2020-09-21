796feb392917990f4a067aa5a5b47d22f1806116
boscoapp <swiftvit53@gmail.com>
2020-09-21 14:01:56 +0300

    no title, new option for listener on two view...

--- app/src/main/java/ir/smartdevelop/eram/showcaseview/MainActivity.java
+++ app/src/main/java/ir/smartdevelop/eram/showcaseview/MainActivity.java
@@ -49,43 +49,43 @@ public class MainActivity extends AppCompatActivity {
                     @Override
                     public void onClick(View v) {
                         Toast.makeText(context, "next", Toast.LENGTH_SHORT).show();
+                        mGuideView.dismiss();
                     }
                 })
                 .setCloseBtnListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
                         Toast.makeText(context, "close", Toast.LENGTH_SHORT).show();
-                        mGuideView.dismiss();
                     }
                 })
                 .setNextBtnColor(R.color.colorAccent)
                 .setCloseBtnColor(R.color.colorPrimaryDark)
                 .setCloseBtnTextTypeFace(Typeface.DEFAULT_BOLD)
                 .setNextBtnTextTypeFace(Typeface.DEFAULT_BOLD)
-                .setTargetView(view1);
-//                .setGuideListener(new GuideListener() {
-//                    @Override
-//                    public void onDismiss(View view) {
-//                        switch (view.getId()) {
-//                            case R.id.view1:
-//                                builder.setTargetView(view2).build();
-//                                break;
-//                            case R.id.view2:
-//                                builder.setTargetView(view3).build();
-//                                break;
-//                            case R.id.view3:
-//                                builder.setTargetView(view4).build();
-//                                break;
-//                            case R.id.view4:
-//                                builder.setTargetView(view5).build();
-//                                break;
-//                            case R.id.view5:
-//                                return;
-//                        }
-//                        mGuideView = builder.build();
-//                        mGuideView.show();
-//                    }
-//                });
+                .setTargetView(view1)
+                .setGuideListener(new GuideListener() {
+                    @Override
+                    public void onDismiss(View view) {
+                        switch (view.getId()) {
+                            case R.id.view1:
+                                builder.setTargetView(view2).build();
+                                break;
+                            case R.id.view2:
+                                builder.setTargetView(view3).build();
+                                break;
+                            case R.id.view3:
+                                builder.setTargetView(view4).build();
+                                break;
+                            case R.id.view4:
+                                builder.setTargetView(view5).build();
+                                break;
+                            case R.id.view5:
+                                return;
+                        }
+                        mGuideView = builder.build();
+                        mGuideView.show();
+                    }
+                });
 
         mGuideView = builder.build();
         mGuideView.show();
